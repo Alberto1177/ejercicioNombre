aa18de64eba49b1c5171a3017ce009c4d0837d23
Alberto Herrera Nieto <alberto.herrera.dev@gmail.com>
2024-09-04 18:48:09 -0600

    Se termino de realizar el ejercicio

--- js/main.js
+++ js/main.js
@@ -1,16 +1,11 @@
+//Variables
 const btnAgregar = document.getElementById("btnAgregar");
-const txtNombre = document.getElementById("Name");
-
-
-
+const txtNombre = document.getElementById("nombre");
 
 const alerValidaciones = document.getElementById("alertValidaciones");
 const alerValidacionesTexto = document.getElementById("alertValidacionesTexto");
 
-
-
-let datos = new Array();
-
+//Evento del boton de agregar
 btnAgregar.addEventListener("click", function(event) {
     event.preventDefault()//prevenir el error
     //Eliminar el mensaje y ocultar el bloque
@@ -19,12 +14,13 @@ btnAgregar.addEventListener("click", function(event) {
     alerValidaciones.style.display = "None";
 
     //Validar el nombre
-    if(txtNombre.value.trim().length<3){
+    if(txtNombre.value.length<3){
         txtNombre.style.border = "solid red medium"
         alerValidacionesTexto.innerHTML  ="El <strong>Nombre</strong> no es correcto. </br>";
         alerValidaciones.style.display ="block";
         return false;
     }
 
-    localStorage.setItem("nombre", );
+    //Guardar el valor en localstorage
+    localStorage.setItem("nombre", txtNombre.value );
 });
--- js/welcome.js
+++ js/welcome.js
@@ -1,22 +1,25 @@
+//Variables
 const btnEliminar = document.getElementById("btnEliminar");
-const mensaje = document.getElementsByTagName("mensajes")
+const mensajes = document.getElementById("mensajes");
+const nombre = localStorage.getItem("nombre");
 
-////////////////////////////
+//Evento del boton de eliminar
 btnEliminar.addEventListener("click", function(event){
     event.preventDefault();
-    txtNombre.value = "";
+    //Eliminar la informacion del contenido del localstorage
     localStorage.clear();
 
-    alerValidaciones.style.display ="none";
-    alerValidacionesTexto.innerHTML ="";
-
-    txtNombre.style.border
+    //Motrar mensaje al eliminar la informacion
+    mensajes.innerHTML = `<p> Se elimino el contenido del localStorage</p>`
 });
 
+//Evento de window
 window.addEventListener("load", function(){
+    //Validar si contiene informacion o no en el localstorage
     if(this.localStorage.getItem("nombre")!=null){
-        mensaje.innerHTML=`Hola ${this.localStorage.getItem("nombre")} bienvenido de nuevo
-        `
+        mensajes.innerHTML = `<p>Hola ${nombre}, bienvenido de nuevo</p>`
+    }else{
+        mensajes.innerHTML = `<p>Por favor ve al index a ingresar tu nombre</p>`
     }
     
-})
+});
